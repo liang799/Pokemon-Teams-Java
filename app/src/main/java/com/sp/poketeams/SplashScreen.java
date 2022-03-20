3a73ffe5e73b58a1e160bd3aae768fa06952b4d5
liang799 <neohtp@tutanota.com>
2022-03-20 17:19:21 +0800

    Minor edit on light mode splash screen

--- app/src/main/java/com/sp/poketeams/SplashScreen.java
+++ app/src/main/java/com/sp/poketeams/SplashScreen.java
@@ -12,12 +12,11 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-
         View decorView = getWindow().getDecorView();
         // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+        setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
         @Override
