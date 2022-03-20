b6e70d4ba9cd0ed462879beea514ca287188a830
liang799 <neohtp@tutanota.com>
2022-03-20 17:00:07 +0800

    Finished Light mode splash screen UI

--- app/src/main/java/com/sp/poketeams/SplashScreen.java
+++ app/src/main/java/com/sp/poketeams/SplashScreen.java
@@ -3,6 +3,7 @@ package com.sp.poketeams;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
         new Handler().postDelayed(new Runnable() {
         @Override
