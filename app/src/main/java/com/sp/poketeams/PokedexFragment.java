d3066f95eb91a34c5133cafc412ea2ed397a7343
liang799 <neohtp@tutanota.com>
2022-03-21 15:51:04 +0800

    Searchview UI done

--- app/src/main/java/com/sp/poketeams/PokedexFragment.java
+++ app/src/main/java/com/sp/poketeams/PokedexFragment.java
@@ -43,90 +43,90 @@ public class PokedexFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_pokedex,container,false);
 
-        pokeview = v.findViewById(R.id.userList);
-
-        customadapter = new Customadapter(getContext(),userListn,userListt);
-        pokeview.setAdapter(customadapter);
-        pokeview.setLayoutManager(new LinearLayoutManager(getContext()));
-
-        new fetchData().start();
+//        pokeview = v.findViewById(R.id.userList);
+//
+//        customadapter = new Customadapter(getContext(),userListn,userListt);
+//        pokeview.setAdapter(customadapter);
+//        pokeview.setLayoutManager(new LinearLayoutManager(getContext()));
+//
+//        new fetchData().start();
 
         return v;
     }
 
-    class fetchData extends Thread {
-
-        String data = "";
-
-
-        @Override
-        public void run(){
-
-            mainHandler.post(new Runnable() {
-                @Override
-                public void run() {
-
-                    /*progressDialog = new ProgressDialog(getContext());
-                    progressDialog.setMessage("Fetching Data");
-                    progressDialog.setCancelable(false);
-                    progressDialog.show();*/
-
-                }
-            });
-
-            try {
-                URL url = new URL("https://pokeapi.co/api/v2/pokemon/zekrom");
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-
-                while((line = bufferedReader.readLine()) != null){
-                    data = data + line;
-                }
-
-                if (!data.isEmpty()){
-                    JSONObject jsonObject = new JSONObject(data);
-                    JSONArray users = jsonObject.getJSONArray("forms");
-                    userListn.clear();
-                    for(int  i =0;i< users.length();i++){
-                        JSONObject names = users.getJSONObject(i);
-                        String name = names.getString("name");
-                        Log.d("ACT1", name);
-                        userListn.add(name);
-                    }
-
-                    JSONArray typesj = jsonObject.getJSONArray("type");
-                    userListt.clear();
-                    for(int  i =0;i< typesj.length();i++){
-                        JSONObject types = typesj.getJSONObject(i);
-                        String type = types.getString("name");
-                        Log.d("ACT1", type);
-                        userListt.add(type);
-
-                    }
-                }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            mainHandler.post(new Runnable() {
-                @Override
-                public void run() {
-
-
-                    customadapter = new Customadapter(getContext(),userListn,userListt);
-                    pokeview.setAdapter(customadapter);
-                    pokeview.setLayoutManager(new LinearLayoutManager(getContext()));
-
-
-                    customadapter.notifyDataSetChanged();
-                }
-            });
-        }
-    }
+//    class fetchData extends Thread {
+//
+//        String data = "";
+//
+//
+//        @Override
+//        public void run(){
+//
+//            mainHandler.post(new Runnable() {
+//                @Override
+//                public void run() {
+//
+//                    /*progressDialog = new ProgressDialog(getContext());
+//                    progressDialog.setMessage("Fetching Data");
+//                    progressDialog.setCancelable(false);
+//                    progressDialog.show();*/
+//
+//                }
+//            });
+//
+//            try {
+//                URL url = new URL("https://pokeapi.co/api/v2/pokemon/zekrom");
+//                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+//                InputStream inputStream = httpURLConnection.getInputStream();
+//                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//                String line;
+//
+//                while((line = bufferedReader.readLine()) != null){
+//                    data = data + line;
+//                }
+//
+//                if (!data.isEmpty()){
+//                    JSONObject jsonObject = new JSONObject(data);
+//                    JSONArray users = jsonObject.getJSONArray("forms");
+//                    userListn.clear();
+//                    for(int  i =0;i< users.length();i++){
+//                        JSONObject names = users.getJSONObject(i);
+//                        String name = names.getString("name");
+//                        Log.d("ACT1", name);
+//                        userListn.add(name);
+//                    }
+//
+//                    JSONArray typesj = jsonObject.getJSONArray("type");
+//                    userListt.clear();
+//                    for(int  i =0;i< typesj.length();i++){
+//                        JSONObject types = typesj.getJSONObject(i);
+//                        String type = types.getString("name");
+//                        Log.d("ACT1", type);
+//                        userListt.add(type);
+//
+//                    }
+//                }
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//
+//            mainHandler.post(new Runnable() {
+//                @Override
+//                public void run() {
+//
+//
+//                    customadapter = new Customadapter(getContext(),userListn,userListt);
+//                    pokeview.setAdapter(customadapter);
+//                    pokeview.setLayoutManager(new LinearLayoutManager(getContext()));
+//
+//
+//                    customadapter.notifyDataSetChanged();
+//                }
+//            });
+//        }
+//    }
 }
